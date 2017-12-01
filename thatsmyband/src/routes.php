bdfd2f3e374663dc71c8058df63ecd5d4539362c
Quentin Midy <q.midy@lectra.com>
2017-12-01 14:03:42 +0100

    Vue User

--- thatsmyband/src/routes.php
+++ thatsmyband/src/routes.php
@@ -19,8 +19,6 @@ $app->get('/user', function (Request $request, Response $response, array $args) 
     // Sample log message
     $this->logger->info("Slim-Skeleton '/user' route");
 
-    $args["expression"] = "Home";
-
     // Appel du User
     $userId = "123";
     $userService = new UserService();
@@ -38,8 +36,6 @@ $app->get('/band', function (Request $request, Response $response, array $args) 
     // Sample log message
     $this->logger->info("Slim-Skeleton '/band' route");
 
-    $args["expression"] = "Band";
-
     // Appel du User
     $userId = "123";
     $userService = new UserService();
@@ -56,8 +52,6 @@ $app->get('/band', function (Request $request, Response $response, array $args) 
 $app->get('/schedule', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/schedule' route");
-
-    $args["expression"] = "Schedule";
 
     // Appel du User
     $userId = "123";
