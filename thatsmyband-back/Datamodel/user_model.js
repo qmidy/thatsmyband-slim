a00c82fa7eae75e7c0277bbc1e20d746418c688b
qmidy <midy.quentin@live.fr>
2018-04-11 23:24:47 +0200

    Suppression de l'ancien back

--- thatsmyband-back/Datamodel/user_model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var userSchema = new Schema({
-   name:String,
-   id:String
-});
-
-module.exports = mongoose.model('users', userSchema);    
